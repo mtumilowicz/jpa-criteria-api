cef5c65fe2dde01741595fedf086fca732061496
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-05-04 23:59:13 +0200

    junit testing

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,61 +1,85 @@
 import com.google.common.base.Preconditions;
 import org.flywaydb.core.Flyway;
 import org.hibernate.Hibernate;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import javax.persistence.*;
 import javax.persistence.criteria.*;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 /**
  * Created by mtumilowicz on 2018-05-02.
  */
-public class Test {
+public class Tests {
     
     private static EntityManagerFactory emf =
             Persistence.createEntityManagerFactory("NewPersistenceUnit");
-
-    public static void main(String[] args) {
+    
+    @BeforeClass
+    public static void prepareForTests() {
+        prepareDatabaseForTests();
+    }
+    
+    private static void prepareDatabaseForTests() {
         Flyway flyway = new Flyway();
         flyway.setDataSource("jdbc:h2:file:./database", null, null);
         flyway.migrate();
     }
     
-    public static List<Book> getAllBooks() {
+    @Test
+    public void getAllBooks() {
         EntityManager entityManager = emf.createEntityManager();
-        
-        entityManager.createQuery("SELECT b FROM Book b", Book.class);
+
+        TypedQuery<Book> jpql_query = 
+                entityManager.createQuery("" +
+                        "SELECT b " +
+                        "FROM Book b", 
+                        Book.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Book> query = cb.createQuery(Book.class);
         Root<Book> query_root = query.from(Book.class);
         query.select(query_root);
         
-        return entityManager.createQuery(query).getResultList();
+        Assert.assertThat(entityManager.createQuery(query).getResultList(), 
+                containsInAnyOrder(jpql_query.getResultList().toArray()));
     }
 
-    public static List<Book> getAllBooksOrderByTitle() {
+    @Test
+    public void getAllBooksOrderByTitle() {
         EntityManager entityManager = emf.createEntityManager();
 
-        entityManager.createQuery("SELECT b FROM Book b ORDER BY b.title", Book.class);
+        TypedQuery<Book> jpql_query = 
+                entityManager.createQuery("" +
+                        "SELECT b " +
+                        "FROM Book b " +
+                        "ORDER BY b.title", 
+                        Book.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Book> query = cb.createQuery(Book.class);
         Root<Book> query_root = query.from(Book.class);
         query.select(query_root);
         query.orderBy(cb.asc(query_root.get("title")));
-        
-        return entityManager.createQuery(query).getResultList();
+
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query.getResultList().toArray()));
     }
     
     public static List<Book> getBooksByTitleLike(String like) {
         EntityManager entityManager = emf.createEntityManager();
 
-        TypedQuery<Book> query1 = entityManager.createQuery("SELECT b " +
+        TypedQuery<Book> jpql_query = entityManager.createQuery("SELECT b " +
                 "FROM Book b " +
                 "WHERE b.title LIKE :like", Book.class);
-        
-        query1.setParameter("like", like);
+
+        jpql_query.setParameter("like", like);
 
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
@@ -122,19 +146,22 @@ public class Test {
         return entityManager.createQuery(query).getResultList();
     }
 
-    public static List<String> getAllBookTitles() {
+    @Test
+    public void getAllBookTitles() {
         EntityManager entityManager = emf.createEntityManager();
 
-        entityManager.createQuery("SELECT b.title " +
-                "FROM Book b", 
-                Book.class);
+        TypedQuery<String> jpql_query = entityManager.createQuery("" +
+                        "SELECT b.title " +
+                        "FROM Book b",
+                String.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<String> query = cb.createQuery(String.class);
         Root<Book> query_root = query.from(Book.class);
-        query.select(query_root.get("name"));
+        query.select(query_root.get("title"));
 
-        return entityManager.createQuery(query).getResultList();
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query.getResultList().toArray()));
     }
 
     public static List<Book> getBooksWithPriceMoreThan(int value) {
@@ -155,45 +182,54 @@ public class Test {
                 .getResultList();
     }
     
-    public static List<Book> getBooksWithMoreThanOneAuthors() {
+    @Test
+    public void getBooksWithMoreThanOneAuthors() {
         EntityManager entityManager = emf.createEntityManager();
 
-        entityManager.createQuery("SELECT b " +
-                "FROM Book b " +
-                "WHERE size(b.authors) > 1", Book.class);
+        TypedQuery<Book> jpql_query = entityManager.createQuery("" +
+                        "SELECT b " +
+                        "FROM Book b " +
+                        "WHERE size(b.authors) > 1",
+                Book.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Book> query = cb.createQuery(Book.class);
         Root<Book> query_root = query.from(Book.class);
-        query.select(query_root.get("name"));
+        query.select(query_root);
         query.where(cb.gt(cb.size(query_root.get("authors")),1));
 
-        return entityManager.createQuery(query).getResultList();
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query.getResultList().toArray()));
     }
     
-    public static long countBooks() {
+    @Test
+    public void countBooks() {
         EntityManager entityManager = emf.createEntityManager();
 
-        entityManager.createQuery("SELECT count(b) " +
-                "FROM Book b", 
-                Book.class);
+        TypedQuery<Long> jpql_query = entityManager.createQuery("" +
+                        "SELECT count(b) " +
+                        "FROM Book b",
+                Long.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Long> query = cb.createQuery(Long.class);
         Root<Book> query_root = query.from(Book.class);
         query.select(cb.count(query_root));
 
-        return entityManager.createQuery(query).getSingleResult();
+        Assert.assertEquals(jpql_query.getSingleResult(), entityManager.createQuery(query).getSingleResult());
     }
     
-    public static List<Tuple> countBooksByGenre() {
+    @Test
+    public void countBooksByGenre() {
         EntityManager entityManager = emf.createEntityManager();
 
-        TypedQuery<Tuple> query1 = 
-                entityManager.createQuery("SELECT " +
+        TypedQuery<Tuple> jpql_query = 
+                entityManager.createQuery("" +
+                        "SELECT " +
                         "b.genre AS bookGenre, count(b) AS bookCount " +
                         "FROM Book b " +
-                        "GROUP BY b.genre", Tuple.class);
+                        "GROUP BY b.genre", 
+                        Tuple.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Tuple> query = cb.createTupleQuery();
@@ -203,16 +239,29 @@ public class Test {
                 cb.count(query_root).alias("count")));
         query.groupBy(query_root.get("genre"));
 
-        return entityManager.createQuery(query).getResultList();
+        List<Tuple> jpqlResultList = jpql_query.getResultList();
+        Set<Object> bookGenre = jpqlResultList.stream().map(x -> x.get("bookGenre")).collect(Collectors.toSet());
+        Set<Object> bookCount = jpqlResultList.stream().map(x -> x.get("bookCount")).collect(Collectors.toSet());
+
+        List<Tuple> resultList = entityManager.createQuery(query).getResultList();
+        Set<Object> genre = resultList.stream().map(x -> x.get("genre")).collect(Collectors.toSet());
+        Set<Object> count = resultList.stream().map(x -> x.get("count")).collect(Collectors.toSet());
+
+
+        Assert.assertEquals(bookGenre, genre);
+        Assert.assertEquals(bookCount, count);
     }
     
-    public static List<WritingGenre> getGenresThatHaveMoreThanOneBook() {
+    @Test
+    public void getGenresThatHaveMoreThanOneBook() {
         EntityManager entityManager = emf.createEntityManager();
 
-        entityManager.createQuery("SELECT b.genre " +
-                "FROM Book b " +
-                "GROUP BY b.genre " +
-                "HAVING count(b.genre) > 1", WritingGenre.class);
+        TypedQuery<WritingGenre> jpql_query = entityManager.createQuery("" +
+                        "SELECT b.genre " +
+                        "FROM Book b " +
+                        "GROUP BY b.genre " +
+                        "HAVING count(b.genre) > 1",
+                WritingGenre.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<WritingGenre> query = cb.createQuery(WritingGenre.class);
@@ -220,8 +269,9 @@ public class Test {
         query.select(query_root.get("genre"));
         query.groupBy(query_root.get("genre"));
         query.having(cb.gt(cb.count(query_root.get("genre")), 1));
-        
-        return entityManager.createQuery(query).getResultList();
+
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query.getResultList().toArray()));
     }
     
     public static List<Book> getBooksByTitle(String title) {
@@ -247,18 +297,21 @@ public class Test {
                 .getResultList();
     }
     
-    public static List<Bookstore> getBookstoresWithAtLeastOneBook() {
+    @Test
+    public void getBookstoresWithAtLeastOneBook() {
         EntityManager entityManager = emf.createEntityManager();
 
-        TypedQuery<Bookstore> query_in = 
-                entityManager.createQuery("SELECT DISTINCT bookstore " +
-                        "FROM Bookstore bookstore, IN(bookstore.books) books", 
-                        // note that without alias 'books' in won't work 
+        TypedQuery<Bookstore> jpql_query_in = 
+                entityManager.createQuery("" +
+                                "SELECT DISTINCT bookstore " + 
+                                "FROM Bookstore bookstore, IN(bookstore.books) books",
+                        // note that without alias 'books' in won't work
                         // (org.hibernate.hql.internal.ast.QuerySyntaxException)
                         Bookstore.class);
 
-        TypedQuery<Bookstore> query_join =
-                entityManager.createQuery("SELECT DISTINCT bookstore " +
+        TypedQuery<Bookstore> jpql_query_join =
+                entityManager.createQuery("" +
+                                "SELECT DISTINCT bookstore " +
                                 "FROM Bookstore bookstore JOIN bookstore.books",
                         Bookstore.class);
 
@@ -268,49 +321,57 @@ public class Test {
         Root<Bookstore> query_root = query.from(Bookstore.class);
         query_root.join("books"); 
         query.select(query_root).distinct(true);
-        
-        return entityManager.createQuery(query).getResultList();
+
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query_in.getResultList().toArray()));
+
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query_join.getResultList().toArray()));
     }
     
-    public static List<Bookstore> getBookstoresWithMostExpensiveBook() {
+    @Test
+    public void getBookstoresWithMostExpensiveBook() {
         EntityManager entityManager = emf.createEntityManager();
 
-        TypedQuery<Bookstore> query = entityManager.createQuery("" + 
+        TypedQuery<Bookstore> jpql_query = entityManager.createQuery("" + 
                 "SELECT book.bookstore " +
                 "FROM Book book " +
                 "WHERE book.price = (SELECT MAX(b.price) FROM Book b)", Bookstore.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Bookstore> cc_query = cb.createQuery(Bookstore.class);
-        Root<Book> query_root = cc_query.from(Book.class);
+        CriteriaQuery<Bookstore> query = cb.createQuery(Bookstore.class);
+        Root<Book> query_root = query.from(Book.class);
         query_root.join("bookstore");
-        cc_query.select(query_root.get("bookstore"));
+        query.select(query_root.get("bookstore"));
 
-        Subquery<Integer> subquery = cc_query.subquery(Integer.class);
+        Subquery<Integer> subquery = query.subquery(Integer.class);
         Root<Book> max_subquery_root = subquery.from(Book.class);
         subquery.select(cb.max(max_subquery_root.get("price")));
 
-        cc_query.where(cb.equal(query_root.get("price"), subquery));
-        
-        return entityManager.createQuery(cc_query).getResultList();
+        query.where(cb.equal(query_root.get("price"), subquery));
+
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query.getResultList().toArray()));
     }
     
-    public static List<Bookstore> getBookstoresFromNewYork() {
+    @Test
+    public void getBookstoresFromNewYork() {
         EntityManager entityManager = emf.createEntityManager();
 
-        TypedQuery<Bookstore> query2 = entityManager.createQuery("" +
+        TypedQuery<Bookstore> jpql_query = entityManager.createQuery("" +
                 "SELECT bookstore " +
                 "FROM Bookstore bookstore " +
                 "WHERE bookstore.address.city = 'New York'", Bookstore.class);
 
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Bookstore> cc_query = cb.createQuery(Bookstore.class);
-        Root<Bookstore> query_root = cc_query.from(Bookstore.class);
-        cc_query.select(query_root);
-        cc_query.where(cb.equal(query_root.get("address").get("city"), "New York"));
-        
-        return entityManager.createQuery(cc_query).getResultList();
+        CriteriaQuery<Bookstore> query = cb.createQuery(Bookstore.class);
+        Root<Bookstore> query_root = query.from(Bookstore.class);
+        query.select(query_root);
+        query.where(cb.equal(query_root.get("address").get("city"), "New York"));
+
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query.getResultList().toArray()));
     }
 
     /**
@@ -385,10 +446,14 @@ public class Test {
     return entityManager.createQuery(c).setParameter("author", author).getResultList();
     }
     
-    public static List<Book> getBooksWithFetchedAuthors() {
+    @Test
+    public void getBooksWithFetchedAuthors() {
         EntityManager entityManager = emf.createEntityManager();
 
-        entityManager.createQuery("SELECT b FROM Book b JOIN FETCH b.authors", Book.class);
+        TypedQuery<Book> jpql_query = entityManager.createQuery("" +
+                        "SELECT b " +
+                        "FROM Book b JOIN FETCH b.authors",
+                Book.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Book> query = cb.createQuery(Book.class);
@@ -400,24 +465,29 @@ public class Test {
         
         Preconditions.checkState(resultList.stream().map(Book::getAuthors).allMatch(Hibernate::isInitialized), 
                 "Not all book.authors are fully initialized!");
-        
-        return resultList;
+
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query.getResultList().toArray()));
     }
     
-    public static List<BookstoreCountAVG> getBookstoresWithCountBooksAndPriceAverage() {
+    @Test
+    public void getBookstoresWithCountBooksAndPriceAverage() {
         EntityManager entityManager = emf.createEntityManager();
 
-        entityManager.createQuery("SELECT new BookstoreCountAVG(b.bookstore, count(b), avg(b.price)) " +
-                "FROM Book b " +
-                "GROUP BY b.bookstore", BookstoreCountAVG.class);
+        TypedQuery<BookstoreCountAVG> jpql_query = entityManager.createQuery("" +
+                        "SELECT new BookstoreCountAVG(b.bookstore, count(b), avg(b.price)) " +
+                        "FROM Book b " +
+                        "GROUP BY b.bookstore",
+                BookstoreCountAVG.class);
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<BookstoreCountAVG> query = cb.createQuery(BookstoreCountAVG.class);
         Root<Book> query_root = query.from(Book.class);
         query.multiselect(query_root.get("bookstore"), cb.count(query_root), cb.avg(query_root.get("price")));
         query.groupBy(query_root.get("bookstore"));
-    
-    return entityManager.createQuery(query).getResultList();
+
+        Assert.assertThat(entityManager.createQuery(query).getResultList(),
+                containsInAnyOrder(jpql_query.getResultList().toArray()));
     }
     
 }
